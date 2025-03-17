0f34527a8bd1213418b2e10fe1b95a8d0c3b9302
solynkaz <solynkaz@yandex.ru>
2025-03-17 17:47:38 +0800

    Added new button to forums

--- dist/sc-opener.user.js
+++ dist/sc-opener.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Open Simpcity Links
 // @namespace    https://github.com/solynkaz-sol
-// @version      0.6.2
+// @version      0.6.3
 // @description  Open Simpcity unread watched threads
 // @updateURL https://github.com/solynkaz-sol/Link-Opener/raw/refs/heads/master/dist/sc-opener.user.js
 // @downloadURL https://github.com/solynkaz-sol/Link-Opener/raw/refs/heads/master/dist/sc-opener.user.js
@@ -14,9 +14,7 @@
 (function() {
   'use strict';
 
-  function openSimpcityLinks(timing = 5750) {
-    const links = document.querySelectorAll('a[href$="/unread"]');
-
+  function openSimpcityLinks(links, timing = 5750) {
     if (links.length > 0) {
       for (const [index, link] of links.entries()) {
         setTimeout(() => {
@@ -45,27 +43,63 @@
     return button;
   }
 
-  function addButtonToHeader() {
-    const header = document.querySelector('.p-body-header .p-title');
-    const links = document.querySelectorAll('a[href$="/unread"]');
-
-    if (header && links.length > 0) {
-      const container = document.createElement('div');
-      container.style.cssText = `
+  function getDivContainer() {
+    const container = document.createElement('div');
+    container.style.cssText = `
             display: flex;
             gap: 8px;
             flex-direction: column;
             justify-content: end;
             padding-bottom: 6px;
         `;
-      const openWithTimingBtn = addButton('Open unseen threads (slow)', () => {openSimpcityLinks()})
-      const openWithoutTiming = addButton('Open unseen threads (instant)', () => {openSimpcityLinks(0)})
 
+    return container;
+  }
+
+  function addWatchedContainer() {
+    const header = document.querySelector('.p-body-header .p-title');
+    const links = document.querySelectorAll('a[href$="/unread"]');
+
+    if (header && links.length > 0) {
+      const container = getDivContainer();
+      const openWithTimingBtn = addButton('Open unseen threads (slow)', () => {openSimpcityLinks(links)})
+      const openWithoutTiming = addButton('Open unseen threads (instant)', () => {openSimpcityLinks(links, 0)})
       container.appendChild(openWithTimingBtn);
       container.appendChild(openWithoutTiming);
+
       header.append(container);
     }
   }
 
-  addButtonToHeader();
+  function addSearchContainer() {
+    const header = document.getElementsByClassName('stickySeparatorbottom')[0]
+    const container = document.createElement('div');
+    container.style.cssText = `
+            display: flex;
+            width: 87.8%;
+            justify-content: end;
+        `;
+    const allLinks = document.querySelectorAll('a');
+
+    const links = Array.from(allLinks).filter(link => {
+      const linkAtt = link.getAttribute('href');
+      return /^\/threads\/[^/]+\/latest$/.test(linkAtt) && !linkAtt.includes('guidelines');
+    }).map(link => link.href);
+
+    const openWithTimingBtn = addButton('Open threads', () => {openSimpcityLinks(links)})
+    container.append(openWithTimingBtn)
+    header.append(container)
+  }
+
+
+  const isWatchedThread = window.location.href.includes("watched/threads");
+  const isForumPage = window.location.href.includes("forums/");
+
+  if (isWatchedThread) {
+    addWatchedContainer();
+  } else if (isForumPage) {
+    addSearchContainer()
+  }
+
+
 })();
